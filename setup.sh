4d08a938bb84fb0bd68a9d5e7663449abfc31f74
HenryDashwood <hcndashwood@gmail.com>
2020-09-02 11:11:56 +0000

    remove -e

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ else
         git clone https://github.com/pyenv/pyenv.git ~/.pyenv
         grep -qxF 'export PYENV_ROOT="$HOME/.pyenv"' ~/.zshrc || echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
         grep -qxF 'export PATH="$PYENV_ROOT/bin:$PATH"' ~/.zshrc || echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-        grep -qxF 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' ~/.zshrc || echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+        grep -qxF 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' ~/.zshrc || echo 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.zshrc
         source ~/.zshrc
         sudo git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
         source ~/.zshrc
