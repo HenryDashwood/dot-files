19dbc8bc03c53dc5fb617e5bfa2b7e21be62a824
Henry Dashwood <hcndashwood@gmail.com>
2021-02-22 11:18:52 +0000

    Update setup_python.sh

--- setup_python.sh
+++ setup_python.sh
@@ -5,10 +5,10 @@ fi
 
 source ~/.zshrc
 
-if [ ! -d ~/.pyenv/versions/3.8.6 ]; then
-        pyenv install 3.8.6
-	pyenv virtualenv 3.8.6 py386
-	pyenv activate py386
+if [ ! -d ~/.pyenv/versions/3.9.1 ]; then
+        pyenv install 3.9.1
+	pyenv virtualenv 3.9.1 py391
+	pyenv activate py391
 	code --install-extension ms-python.python
 	code --install-extension ms-python.vscode-pylance
 fi
