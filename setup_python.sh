a0c5da31924937cdca626a758edad02ffe7aff2c
Henry Dashwood <hcndashwood@gmail.com>
2021-03-02 03:31:13 +0000

    Update setup_python.sh

--- setup_python.sh
+++ setup_python.sh
@@ -3,6 +3,8 @@ if [ ! -d ~/.pyenv ]; then
         git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 fi
 
+source ~/.zshrc
+
 if [ ! -d ~/.pyenv/versions/3.9.1 ]; then
         pyenv install 3.9.1
 	pyenv virtualenv 3.9.1 py391
