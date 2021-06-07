fa1f80a9e700db0f4af1680d602683ce6df6422c
Henry Dashwood <hcndashwood@gmail.com>
2021-06-07 11:58:44 +0100

    Update install_docker.sh

--- remote/install_docker.sh
+++ remote/install_docker.sh
@@ -1,12 +1,10 @@
-sudo apt update
-sudo apt install curl apt-transport-https ca-certificates software-properties-common
+sudo apt update -y
+sudo apt install -y curl apt-transport-https ca-certificates software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
