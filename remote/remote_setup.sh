537077ee0b38169afe9578bcd6087be3513e9ea1
HenryDashwood <hcndashwood@gmail.com>
2024-01-16 16:26:37 +0000

    update remote and python setups

--- remote/remote_setup.sh
+++ remote/remote_setup.sh
@@ -4,13 +4,13 @@ IP=$3
 PRIVATE_KEY=$4
 
 function createsshuser() {
-	useradd -m -s /bin/bash $1
-	passwd $1
+	useradd -m $1
 	usermod -aG sudo $1
-	echo "$1 ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/toto
 	mkdir /home/$1/.ssh
-	ssh-keyscan -t rsa github.com >> /home/$1/.ssh/known_hosts
-	chmod -R 777 /home/$1/.ssh
+	cp ~/.ssh/authorized_keys /home/$1/.ssh/
+	chown -R $1:$1 /home/$1
+	chmod 700 /home/$1/.ssh
+	chmod 600 /home/$1/.ssh/authorized_keys
 }
 
 function security() {
@@ -18,13 +18,14 @@ function security() {
 	sudo -S ufw allow ssh
 	sudo -S ufw --force enable
 	sudo -S ufw allow 8888
+
 }
 
 function setupzsh() {
-	sudo -S apt install -y \
-		build-essential cmake curl git \
-		libbz2-dev libncurses5-dev liblzma-dev libncursesw5-dev libffi-dev libreadline-dev libsqlite3-dev libssl-dev \
-		llvm make protobuf-compiler python-openssl tk-dev unzip wget xz-utils zlib1g-dev zsh
+	sudo -S apt-get install -y \
+		build-essential cmake curl git libbz2-dev libncurses5-dev liblzma-dev libncursesw5-dev libffi-dev \
+		libreadline-dev libsqlite3-dev libssl-dev libxmlsec1-dev libxml2-dev llvm make protobuf-compiler \
+		python3-openssl tk-dev unzip wget xz-utils zlib1g-dev zsh
 
 	git config --global user.email hcndashwood@gmail.com
 	git config --global user.name HenryDashwood
@@ -48,23 +49,23 @@ function setuppython() {
 
 	source ~/.zshrc
 
-	if [ ! -d ~/.pyenv/versions/3.11.6 ]; then
-		pyenv install 3.11.6
-		pyenv virtualenv 3.11.6 py3116
-		/home/$USERNAME/.pyenv/versions/3.11.6/envs/py3116/bin/python3.11 -m pip install --upgrade pip wheel
+	if [ ! -d ~/.pyenv/versions/3.11.7 ]; then
+		pyenv install 3.11.7
+		pyenv virtualenv 3.11.7 py3117
+		/home/$USERNAME/.pyenv/versions/3.11.7/envs/py3117/bin/python3.11 -m pip install --upgrade pip wheel
 		echo 'export PYENV_ROOT="$HOME/.pyenv"' >> .profile
 		echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> .profile
 		echo 'eval "$(pyenv init --path)"' >> .profile
 		echo 'export PYENV_ROOT="$HOME/.pyenv"' >> .zprofile
 		echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> .zprofile
 		echo 'eval "$(pyenv init --path)"' >> .zprofile
-		pyenv activate py3116
+		pyenv activate py3117
 	fi
 }
 
 if [ $PROVIDER == "EC2" ]
 then {
-	scp -i $PRIVATE_KEY ~/.ssh/id_ed25519 $USERNAME@$IP:~/.ssh
+	scp -i $PRIVATE_KEY $USERNAME@$IP:~/.ssh
 	ssh -i $PRIVATE_KEY $USERNAME@$IP "chmod 400 /home/$USERNAME/.ssh/id_ed25519"
 	ssh -i $PRIVATE_KEY $USERNAME@$IP "sudo -S apt update -y"
 	ssh -i $PRIVATE_KEY $USERNAME@$IP "ssh-keyscan github.com >> ~/.ssh/known_hosts"
@@ -76,13 +77,17 @@ then {
 elif [ $PROVIDER == "DATACRUNCH" ] 
 then {
 	ssh -i $PRIVATE_KEY root@$IP "$(typeset -f createsshuser); createsshuser $USERNAME"
-	scp ~/.ssh/id_ed25519 $USERNAME@$IP:~/.ssh
-	ssh $USERNAME@$IP "chmod 400 /home/$USERNAME/.ssh/datacrunch"
-	ssh $USERNAME@$IP "sudo -S apt update -y"
-	ssh $USERNAME@$IP "$(typeset -f security); security"
-	scp -r ../configs/shell/.zshrc $USERNAME@$IP:~/.zshrc
-	scp -r ../configs/shell/.vimrc $USERNAME@$IP:~/.vimrc
-	ssh $USERNAME@$IP "$(typeset -f setupzsh); setupzsh $USERNAME"
-	ssh $USERNAME@$IP "$(typeset -f setuppython); setuppython"
+	ssh -i $PRIVATE_KEY $USERNAME@$IP "sudo -S apt update -y"
+
+	scp -i $PRIVATE_KEY ~/.ssh/id_ed25519 $USERNAME@$IP:~/.ssh
+	ssh -i $PRIVATE_KEY $USERNAME@$IP "chmod 400 /home/$USERNAME/.ssh/id_ed25519"
+
+	ssh -i $PRIVATE_KEY $USERNAME@$IP "$(typeset -f security); security"
+
+	scp -i $PRIVATE_KEY -r ../configs/shell/.zshrc $USERNAME@$IP:~/.zshrc
+	scp -i $PRIVATE_KEY -r ../configs/shell/.vimrc $USERNAME@$IP:~/.vimrc
+
+	ssh -i $PRIVATE_KEY $USERNAME@$IP "$(typeset -f setupzsh); setupzsh $USERNAME"
+	ssh -i $PRIVATE_KEY $USERNAME@$IP "$(typeset -f setuppython); setuppython"
 }
 fi
